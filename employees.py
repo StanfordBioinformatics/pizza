997af56bfefa3b08350ee11d091e99463480b026
Nathaniel Watson <nathan.watson86@gmail.com>
2015-05-28 19:15:06 -0700

    m

--- employees.py
+++ employees.py
@@ -16,7 +16,7 @@ class Chef(Employee):
     def __init__(self, name):
         Employee.__init__(self, name, 50000)
     def work(self):
-        print(self.name, "makes lots of food")
+        print(self.name, "makes lots of great food")
 class Server(Employee):
     def __init__(self, name):
         Employee.__init__(self, name, 40000)
