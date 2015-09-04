80ea6a624611a6666dfff6395ef56466459744fa
Nathaniel Watson <nathankw@stanford.edu>
2015-09-04 11:50:59 -0700

    Updated employees.py

--- employees.py
+++ employees.py
@@ -7,7 +7,7 @@ class Employee:
         self.salary = salary
     def giveRaise(self, percent):
         self.salary = self.salary + (self.salary * percent)
-    def work(self,time,what):
+    def work(self):
         print(self.name, "does stuff")
     def __repr__(self):
         return "<Employee: name=%s, salary=%s>" % (self.name, self.salary)
