3f7332a4f331bda760eca5c26e219279069fd05a
Nathaniel Watson <nathankw@stanford.edu>
2015-09-04 11:55:18 -0700

    Added 'what' argument to Employee work method.

--- employees.py
+++ employees.py
@@ -7,8 +7,8 @@ class Employee:
         self.salary = salary
     def giveRaise(self, percent):
         self.salary = self.salary + (self.salary * percent)
-    def work(self):
-        print(self.name, "does stuff")
+    def work(self,work):
+        print(self.name, "does {what}".format(what=what))
     def __repr__(self):
         return "<Employee: name=%s, salary=%s>" % (self.name, self.salary)
 
